ab6b0fe4ecef91d06ec52b10dba4dab7952afafd
Ploynapa Kham <ploynp.k@outlook.com>
2025-02-04 23:10:01 -0800

    cleaned code and added images

--- src/client/components/Report.jsx
+++ src/client/components/Report.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import DataContext from "../data/DataContext";
 
 
-// 
 const Report = () => {
     const {lent , borrowed} = useContext(DataContext);
     return (
